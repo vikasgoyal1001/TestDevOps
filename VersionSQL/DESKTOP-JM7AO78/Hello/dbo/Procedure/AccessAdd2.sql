a4b70b14f362e0d752222f5fbe074f159610fb4e
Vikas Goyal <vikas.goyal@conractpodai.com>
2020-01-21 18:44:36 +0530

    Added 2

---
+++ VersionSQL/DESKTOP-JM7AO78/Hello/dbo/Procedure/AccessAdd2.sql
@@ -0,0 +1,10 @@
+/****** Object:  Procedure [dbo].[AccessAdd2]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[AccessAdd2]
+
+AS
+BEGIN
+
+	select 'Hello vikas2'
+END
+
