ffd6a7e9a512fe021fc784e967355c1eaa3ecfbf
Vikas Goyal <vikas.goyal@conractpodai.com>
2020-01-21 18:41:35 +0530

    Added third proc

---
+++ VersionSQL/DESKTOP-JM7AO78/Hello/dbo/Procedure/AccessAdd3.sql
@@ -0,0 +1,9 @@
+/****** Object:  Procedure [dbo].[AccessAdd3]    Committed by VersionSQL https://www.versionsql.com ******/
+
+CREATE PROCEDURE [dbo].[AccessAdd3]
+
+AS
+BEGIN
+
+	select 'Hello vikas3'
+END
